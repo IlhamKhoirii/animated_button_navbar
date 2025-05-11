792eb191bdb845afeaf3e373f7c11b5c8b5c008e
Ilham Khoiri <230103063@mhs.udb.ac.id>
2025-05-11 22:45:13 +0700

    Remove import statement for ilham_screen.dart in login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'ilham_screen.dart';
+// import 'ilham_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
